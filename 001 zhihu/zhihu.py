868d498fd9d97449ab09e6a4ec177273190c8cd3
YOYOXUE <ariaxyd@foxmail.com>
2017-01-03 20:39:56 +0800

    Update zhihu.py

--- 001 zhihu/zhihu.py
+++ 001 zhihu/zhihu.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# python3
+# -*- coding: utf-8
 '''
-Required
-- requests (必须)
-- pillow (可选)
-Info
-- author : "xchaoinfo"
-- email  : "xchaoinfo@qq.com"
-- date   : "2016.2.4"
-Update
-- name   : "wangmengcn"
-- email  : "eclipse_sv@163.com"
-- date   : "2016.4.21"
 '''
 import requests
 try:
@@ -27,7 +16,7 @@ except:
     pass
 
 
-# 构造 Request headers
+# Request headers
 agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
 headers = {
     "Host": "www.zhihu.com",
@@ -35,28 +24,28 @@ headers = {
     'User-Agent': agent
 }
 
-# 使用登录cookie信息
+# cookie
 session = requests.session()
 session.cookies = cookielib.LWPCookieJar(filename='cookies')
 try:
     session.cookies.load(ignore_discard=True)
 except:
-    print("Cookie 未能加载")
+    print("Cookie unload")
 
 
 def get_xsrf():
-    '''_xsrf 是一个动态变化的参数'''
+    '''_xsrf variable'''
     index_url = 'https://www.zhihu.com'
-    # 获取登录时需要用到的_xsrf
+    # get_xsrf
     index_page = session.get(index_url, headers=headers)
     html = index_page.text
     pattern = r'name="_xsrf" value="(.*?)"'
-    # 这里的_xsrf 返回的是一个list
+    # list
     _xsrf = re.findall(pattern, html)
     return _xsrf[0]
 
 
-# 获取验证码
+# verification code
 def get_captcha():
     t = str(int(time.time() * 1000))
     captcha_url = 'https://www.zhihu.com/captcha.gif?r=' + t + "&type=login"
@@ -64,20 +53,20 @@ def get_captcha():
     with open('captcha.jpg', 'wb') as f:
         f.write(r.content)
         f.close()
-    # 用pillow 的 Image 显示验证码
-    # 如果没有安装 pillow 到源代码所在的目录去找到验证码然后手动输入
+    # pillow 
+    
     try:
         im = Image.open('captcha.jpg')
         im.show()
         im.close()
     except:
-        print(u'请到 %s 目录找到captcha.jpg 手动输入' % os.path.abspath('captcha.jpg'))
+        print(u'Please %s find captcha.jpg and input it' % os.path.abspath('captcha.jpg'))
     captcha = input("please input the captcha\n>")
     return captcha
 
 
 def isLogin():
-    # 通过查看用户个人信息来判断是否已经登录
+   
     url = "https://www.zhihu.com/settings/profile"
     login_code = session.get(url, headers=headers, allow_redirects=False).status_code
     if login_code == 200:
@@ -87,9 +76,9 @@ def isLogin():
 
 
 def login(secret, account):
-    # 通过输入的用户名判断是否是手机号
+    # 
     if re.match(r"^1\d{10}$", account):
-        print("手机号登录 \n")
+        print("phone number \n")
         post_url = 'https://www.zhihu.com/login/phone_num'
         postdata = {
             '_xsrf': get_xsrf(),
@@ -99,9 +88,9 @@ def login(secret, account):
         }
     else:
         if "@" in account:
-            print("邮箱登录 \n")
+            print("email address \n")
         else:
-            print("你的账号输入有问题，请重新登录")
+            print("Your username is in correct")
             return 0
         post_url = 'https://www.zhihu.com/login/email'
         postdata = {
@@ -111,13 +100,13 @@ def login(secret, account):
             'email': account,
         }
     try:
-        # 不需要验证码直接登录成功
+        # no verification code
         login_page = session.post(post_url, data=postdata, headers=headers)
         login_code = login_page.text
         print(login_page.status_code)
         print(login_code)
     except:
-        # 需要输入验证码后才能登录成功
+        # verification
         postdata["captcha"] = get_captcha()
         login_page = session.post(post_url, data=postdata, headers=headers)
         login_code = eval(login_page.text)
@@ -132,8 +121,8 @@ except:
 
 if __name__ == '__main__':
     if isLogin():
-        print('您已经登录')
+        print('logged in')
     else:
-        account = input('请输入你的用户名\n>  ')
-        secret = input("请输入你的密码\n>  ")
+        account = input('username\n>  ')
+        secret = input("pasword\n>  ")
         login(secret, account)
